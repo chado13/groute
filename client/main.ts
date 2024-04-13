0351a00217ab9aa431a378b5016411cfa8858098
chado13 <geaya13@gmail.com>
2024-04-13 16:32:51 +0900

    build: client setting

---
+++ client/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import App from './app.vue'
+// import PrimeVue from 'primevue/config';
+// import 'primevue/resources/themes/saga-blue/theme.css';
+// import 'primevue/resources/primevue.min.css';
+// import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+// app.use(PrimeVue);
+app.mount('#app');
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,10 +1,9 @@
-
 export default defineNuxtConfig({
   modules: [
       'nuxt-primevue'
   ],
   primevue: {
     cssLayerOrder: 'reset,primevue'
-  },
+},
   css: ['primevue/resources/themes/aura-light-green/theme.css']
 })
