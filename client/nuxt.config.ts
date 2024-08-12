5bbb11afd9e1ac44f93f81d3e55eb5ed32cb5f9b
chado13 <geaya13@gmail.com>
2024-08-13 07:10:03 +0900

    chore: kakao api 추가

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,14 +1,13 @@
 import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
-  modules: [
-      'nuxt-primevue'
-  ],
+  modules: ['nuxt-primevue'],
   primevue: {
     cssLayerOrder: 'reset,primevue'
   },
   runtimeConfig: {
     public: {
       mapApi: process.env.MAP_API,
+      kakaomapApi: process.env.KAKAO_API_KEY,
     },
   },
   css: ['primevue/resources/themes/aura-light-green/theme.css']
