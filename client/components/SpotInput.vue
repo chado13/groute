d52d00c3384d03bc93caf4bfe43ebbe5ddb5647e
chado13 <geaya13@gmail.com>
2024-08-13 07:09:07 +0900

    feat: 관광지 어레이 푸쉬

---
+++ client/components/SpotInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex">
+    <InputText v-model="value" />
+    <Button label="입력" @click="onClick" />
+  </div>
+  <div>
+    <Chip v-for="spot in props.spots" :key="spot" :label="spot" />
+  </div>
+</template>
+<script setup lang="ts">
+const emit = defineEmits<{
+  "spot-added": [string];
+}>();
+
+const props = defineProps<{
+  spots: string[];
+}>();
+
+const value = ref("");
+
+function onClick() {
+  console.log(value.value);
+  emit("spot-added", value.value);
+  value.value = "";
+}
+</script>
