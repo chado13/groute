2e297db1c3e9786cbf8f974be512e3d60e10e912
chado13 <geaya13@gmail.com>
2024-09-24 21:07:16 +0900

    chore: 아직 사용안하는 configure 주석

--- server/app/config.py
+++ server/app/config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     # gptapi_key: str
-    tour_api_key: str
-    kakao_map_api_key: str
+    # tour_api_key: str
+    # kakao_map_api_key: str
     tour_api_service_key: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
