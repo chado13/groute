4a130b5d888b52629a66f2f3b3838eebe49a70ca
chado13 <geaya13@gmail.com>
2024-09-27 20:13:25 +0900

    chore: console 추가

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,31 +1,52 @@
 <template>
-
   <head>
-    <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' rel='stylesheet' type='text/css'>
-    <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-jp.css' rel='stylesheet' type='text/css'>
+    <link
+      href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-jp.css"
+      rel="stylesheet"
+      type="text/css"
+    />
   </head>
   <div>
     <!-- Step 1 -->
     <div v-if="step === 1">
       <div class="header-container">
-        <svg width="114" height="24" viewBox="0 0 114 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          width="114"
+          height="24"
+          viewBox="0 0 114 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g clip-path="url(#clip0_23_91)">
             <path
               d="M40.4377 6.5549C40.4228 2.90798 37.3474 0.381348 32.8636 0.381348H23.688V20.5408H27.7202V13.1396H31.7196L36.459 20.5408H41.0349L35.9152 12.7016C38.6786 11.8286 40.4228 9.32883 40.4377 6.5549ZM33.0835 9.75192H27.7231V4.06999H33.0835C35.1397 4.06999 36.3669 5.06813 36.3788 6.71878C36.3639 8.37241 35.1397 9.76384 33.0835 9.75192Z"
-              fill="white" />
+              fill="white"
+            />
             <path
               d="M75.3485 11.9628C75.3336 15.1717 73.7677 17.0995 71.1796 17.0995C68.5916 17.0995 67.0257 15.1747 67.0405 11.9628V0.765747H63.0084V11.9657C62.9935 17.3468 66.265 20.9252 71.1796 20.9252C76.0943 20.9252 79.3658 17.3468 79.3777 11.9657V0.765747H75.3455V11.9657L75.3485 11.9628Z"
-              fill="white" />
-            <path d="M82.2656 4.56165H88.1757V20.5408H92.2079V4.56165H98.0645V0.765747H82.2656V4.56165Z" fill="white" />
+              fill="white"
+            />
+            <path
+              d="M82.2656 4.56165H88.1757V20.5408H92.2079V4.56165H98.0645V0.765747H82.2656V4.56165Z"
+              fill="white"
+            />
             <path
               d="M105.092 16.7449V12.1296H113.019V8.65848H105.092V4.56165H113.78V0.765747H101.06V20.5408H114V16.7449H105.092Z"
-              fill="white" />
+              fill="white"
+            />
             <path
               d="M10.3256 4.39778C10.4652 4.39778 10.599 4.40076 10.7356 4.4097H19.7805V0.584001H10.7356C10.599 0.578042 10.4652 0.572083 10.3256 0.572083C4.42739 0.575062 -0.0148197 4.99964 3.71562e-05 10.8455C-0.0148197 16.6913 4.43928 21.1158 10.3523 21.1158C15.3324 21.1158 19.0793 17.9993 19.6884 13.3036C19.7449 12.8626 19.7775 12.4067 19.7775 11.9389V9.94564H10.7327V13.3065H15.6622C15.1303 15.7527 13.1692 17.2931 10.3494 17.2931C6.64405 17.2931 4.04409 14.6294 4.05597 10.8455C4.04112 7.06147 6.62919 4.39778 10.3196 4.39778H10.3256Z"
-              fill="white" />
+              fill="white"
+            />
             <path
               d="M51.3189 0C47.26 0 43.2041 3.47412 43.2041 9.2365C43.2041 14.9989 51.3189 24.003 51.3189 24.003C51.3189 24.003 59.4338 14.9989 59.4338 9.2365C59.4338 3.47412 55.3779 0 51.3189 0ZM51.3189 13.2708C48.7962 13.2708 46.7519 11.2209 46.7519 8.69125C46.7519 6.16164 48.7962 4.11173 51.3189 4.11173C53.8416 4.11173 55.886 6.16164 55.886 8.69125C55.886 11.2209 53.8416 13.2708 51.3189 13.2708Z"
-              fill="white" />
+              fill="white"
+            />
           </g>
           <defs>
             <clipPath id="clip0_23_91">
@@ -36,9 +57,19 @@
       </div>
       <h2 class="step-title">어떤 도시로 떠나볼까요?</h2>
       <div class="custom-input-container">
-        <InputText class="custom-input" style="margin-left: 40px;" v-model="formData.destination"
-          placeholder="도시 이름을 입력해 주세요." @keyup.enter="nextStep" />
-        <Button class="custom-button" :disabled="!formData.destination" @click="nextStep">다음</Button>
+        <InputText
+          class="custom-input"
+          style="margin-left: 40px"
+          v-model="formData.destination"
+          placeholder="도시 이름을 입력해 주세요."
+          @keyup.enter="nextStep"
+        />
+        <Button
+          class="custom-button"
+          :disabled="!formData.destination"
+          @click="nextStep"
+          >다음</Button
+        >
       </div>
     </div>
 
@@ -49,11 +80,36 @@
       </div>
       <h2 class="step-title">여행 일정을 알려주세요.</h2>
       <div class="custom-input-container">
-        <Calendar class="calendar-input" v-model="formData.start" showButtonBar dateFormat="yy-mm-dd"
-          placeholder="출발일" />
-        <p style="text-align: center; line-height: 3; width:5%; font-weight: 600;">-</p>
-        <Calendar class="calendar-input" v-model="formData.end" showButtonBar dateFormat="yy-mm-dd" placeholder="도착일" />
-        <Button class="custom-button" :disabled="!formData.start || !formData.end" @click="nextStep">다음</Button>
+        <Calendar
+          class="calendar-input"
+          v-model="formData.start"
+          showButtonBar
+          dateFormat="yy-mm-dd"
+          placeholder="출발일"
+        />
+        <p
+          style="
+            text-align: center;
+            line-height: 3;
+            width: 5%;
+            font-weight: 600;
+          "
+        >
+          -
+        </p>
+        <Calendar
+          class="calendar-input"
+          v-model="formData.end"
+          showButtonBar
+          dateFormat="yy-mm-dd"
+          placeholder="도착일"
+        />
+        <Button
+          class="custom-button"
+          :disabled="!formData.start || !formData.end"
+          @click="nextStep"
+          >다음</Button
+        >
       </div>
     </div>
 
@@ -65,18 +121,40 @@
       <h2 class="step-title">꼭 방문하고 싶은 장소를 추가해 주세요.</h2>
       <div class="custom-input-container">
         <div class="input-wrapper">
-          <InputText v-model="spot" class="custom-input" @keyup.enter="searchSpot" placeholder="장소명을 입력해 주세요."
-            @focus="isFocused = true" @blur="isFocused = false" />
-          <i class="pi pi-arrow-right search-icon" :class="{ 'icon-activate': isFocused }" @click="searchSpot"></i>
+          <InputText
+            v-model="spot"
+            class="custom-input"
+            @keyup.enter="searchSpot"
+            placeholder="장소명을 입력해 주세요."
+            @focus="isFocused = true"
+            @blur="isFocused = false"
+          />
+          <i
+            class="pi pi-arrow-right search-icon"
+            :class="{ 'icon-activate': isFocused }"
+            @click="searchSpot"
+          ></i>
         </div>
         <div class="input-wrapper">
-          <Chip v-for="spot in formData.spots" :key="spot.name" :label="spot.name" removable @remove="removeSpot(spot)"
-            class="chip-item" />
+          <Chip
+            v-for="spot in formData.spots"
+            :key="spot.name"
+            :label="spot.name"
+            removable
+            @remove="removeSpot(spot)"
+            class="chip-item"
+          />
         </div>
-        <div class="recommandation" v-if="lastChipItem.trim() !== '' && spot.trim() === ''">
-          <p style="margin: 20px;"> {{ lastChipItem }} 주변 추천</p>
+        <div
+          class="recommandation"
+          v-if="lastChipItem.trim() !== '' && spot.trim() === ''"
+        >
+          <p style="margin: 20px">{{ lastChipItem }} 주변 추천</p>
           <div class="datatable-container">
-            <DataTable :value="recommandationData" style="margin: 25px; align-self: auto;">
+            <DataTable
+              :value="recommandationData"
+              style="margin: 25px; align-self: auto"
+            >
               <Column field="name" header="장소"></Column>
               <Column field="dist" header="거리">
                 <template #body="data">
@@ -86,10 +164,13 @@
             </DataTable>
           </div>
         </div>
-        <Button class="custom-button" :disabled="formData.spots.length === 0" @click="nextStep">다음</Button>
+        <Button
+          class="custom-button"
+          :disabled="formData.spots.length === 0"
+          @click="nextStep"
+          >다음</Button
+        >
       </div>
-
-
     </div>
   </div>
   <!-- Step 4 -->
@@ -98,23 +179,52 @@
       <i class="pi pi-angle-left prev-step-icon" @click="prevStep"></i>
     </div>
     <div class="input-wrapper">
-      <h2 class="step-title" style="margin-left: 0px;">여행 시작 장소를 알려주세요.</h2>
-      <InputText v-model="arrival" class="custom-input" :class="{ 'input-activate': arrival || isArrivalFocused }"
-        @focus="isArrivalFocused = true" @blur="isArrivalFocused = false" @keyup.enter="searchArrival"
-        placeholder="여행지 내에서 여행을 시작할 장소   EX:강릉역" />
-      <i class="pi pi-arrow-right search-icon" style="top:70%" :class="{ 'icon-activate': isArrivalFocused }"
-        @click="searchSpot"></i>
+      <h2 class="step-title" style="margin-left: 0px">
+        여행 시작 장소를 알려주세요.
+      </h2>
+      <InputText
+        v-model="arrival"
+        class="custom-input"
+        :class="{ 'input-activate': arrival || isArrivalFocused }"
+        @focus="isArrivalFocused = true"
+        @blur="isArrivalFocused = false"
+        @keyup.enter="searchArrival"
+        placeholder="여행지 내에서 여행을 시작할 장소   EX:강릉역"
+      />
+      <i
+        class="pi pi-arrow-right search-icon"
+        style="top: 70%"
+        :class="{ 'icon-activate': isArrivalFocused }"
+        @click="searchSpot"
+      ></i>
     </div>
     <div class="input-wrapper">
-      <h2 class="step-title" style="margin-left: 0px;">여행 종료 장소를 알려주세요.</h2>
-      <InputText v-model="depart" class="custom-input" :class="{ 'input-activate': depart || isDepartFocused }"
-        @focus="isDepartFocused = true" @blur="isDepartFocused = false" @keyup.enter="searchDepart"
-        placeholder="여행지 내에서 여행을 종료할 장소   EX:강릉역" />
-      <i class="pi pi-arrow-right search-icon" style="top:70%" :class="{ 'icon-activate': isDepartFocused }"
-        @click="searchSpot"></i>
+      <h2 class="step-title" style="margin-left: 0px">
+        여행 종료 장소를 알려주세요.
+      </h2>
+      <InputText
+        v-model="depart"
+        class="custom-input"
+        :class="{ 'input-activate': depart || isDepartFocused }"
+        @focus="isDepartFocused = true"
+        @blur="isDepartFocused = false"
+        @keyup.enter="searchDepart"
+        placeholder="여행지 내에서 여행을 종료할 장소   EX:강릉역"
+      />
+      <i
+        class="pi pi-arrow-right search-icon"
+        style="top: 70%"
+        :class="{ 'icon-activate': isDepartFocused }"
+        @click="searchSpot"
+      ></i>
     </div>
 
-    <Button class="custom-button" :disabled="!formData.depart || !formData.arrival" @click="submitForm">경로 검색</Button>
+    <Button
+      class="custom-button"
+      :disabled="!formData.depart || !formData.arrival"
+      @click="submitForm"
+      >경로 검색</Button
+    >
   </div>
   <ProgressSpinner class="progress-spinner" v-if="isLoading" />
   <ClientOnly>
@@ -179,7 +289,7 @@ const prevStep = () => {
     step.value--;
   }
 };
-const lastChipItem = ref('')
+const lastChipItem = ref("");
 const searchPlaceAndAssign = useDebounceFn(async (keywordSource, field) => {
   const keyword = keywordSource.value;
   if (!keyword) return;
@@ -189,7 +299,7 @@ const searchPlaceAndAssign = useDebounceFn(async (keywordSource, field) => {
     if (field == "spots") {
       formData.value.spots.push(placeItem);
       spot.value = "";
-      lastChipItem.value = keyword
+      lastChipItem.value = keyword;
       await recomandation(placeItem);
     } else {
       formData.value[field] = placeItem;
@@ -244,14 +354,11 @@ const visible = computed({
     resultData.value = [];
   },
 });
-const apiUrl = process.env.NUXT_SERVER_URL
+const apiUrl = process.env.NUXT_SERVER_URL;
 const submitForm = async () => {
   try {
     isLoading.value = true;
-    const response = await axios.post(
-      `${apiUrl}/groute/route`,
-      formData.value
-    );
+    const response = await axios.post(`${apiUrl}/groute/route`, formData.value);
     console.log("폼 데이터가 성공적으로 전송되었습니다:", response.data);
     resultData.value = response.data;
   } catch (error) {
@@ -261,26 +368,29 @@ const submitForm = async () => {
     isLoading.value = false;
   }
 };
-const recommandLoading = ref(false)
-const recommandationData = ref([])
+const recommandLoading = ref(false);
+const recommandationData = ref([]);
 async function recomandation(spot: placeItem) {
-  console.log(spot)
+  console.log(`${apiUrl}`);
   try {
-    recommandLoading.value = true
-    const response = await axios.get(
-      `${apiUrl}/groute/places`, { params: { lng: parseFloat(spot.lng), lat: parseFloat(spot.lat), redius: 500 } }
-    );
-    console.log("추천장소 요청", response.data)
-    recommandationData.value = response.data
-
+    recommandLoading.value = true;
+    const response = await axios.get(`${apiUrl}/groute/places`, {
+      params: {
+        lng: parseFloat(spot.lng),
+        lat: parseFloat(spot.lat),
+        redius: 500,
+      },
+    });
+    console.log("추천장소 요청", response.data);
+    recommandationData.value = response.data;
   } catch (error) {
     throw error;
   } finally {
-    recommandLoading.value = false
+    recommandLoading.value = false;
   }
-};
+}
 const distanceCeil = (data) => {
-  console.log(data)
+  console.log(data);
   return Math.ceil(parseFloat(data.dist)).toLocaleString();
 };
 </script>
